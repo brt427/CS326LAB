ec2a5ec4184096fdb283b7cbc8fb809d9021ef8a
Tri Than <thantrongtri3@gmail.com>
2024-10-15 20:18:06 -0400

    add test for tangent function

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,30 +2,33 @@ const assert = require('assert');
 const calculator = require('../calculator');
 
 // Test memory functions
-describe('Calculator Memory Functions', function() {
-    
-});
-
+describe('Calculator Memory Functions', function () {});
 
 // Test calculation functions
-describe('Calculator Calculation Functions', function() {
-    const res = calculator.calculateSquareRoot(4);
-    it('should return 2', function() {
-        assert.strictEqual(res, 2);
-    });
+describe('Calculator Calculation Functions', function () {
+  const res = calculator.calculateSquareRoot(4);
+  it('should return 2', function () {
+    assert.strictEqual(res, 2);
+  });
 });
 
+describe('tan function', function () {
+  it('tan(2) should be close to 0.017455064', function () {
+    const res = calculator.tangent(1);
+    assert.closeTo(res, 0.017455064, 1e-8);
+  });
+});
 
-describe('square function', function() {
+describe('square function', function () {
   const res = calculator.calculateSquare(3);
-  it('should return 9', function() {
+  it('should return 9', function () {
     assert.strictEqual(res, 9);
   });
 });
 
-describe('square root function', function() {
+describe('square root function', function () {
   const res = calculator.calculateSquareRoot(9);
-  it('should return 3', function() {
+  it('should return 3', function () {
     assert.strictEqual(res, 3);
   });
 });
